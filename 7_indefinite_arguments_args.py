3a0e0ff565717923e8f5fbb62f8e2a1548123ccd
LBarocio <lbarocio1@cps.edu>
2025-01-14 12:13:30 -0600

    Add files via upload

---
+++ 7_indefinite_arguments_args.py
@@ -0,0 +1,80 @@
+# *args = allows you to pass multiple non-key arguements 
+#  **kwargs = allow you to pass multiple keyword- arguments 
+            # *unpacking operator 
+#           1. positional 2. default 3. keyword 4. ARBITRARY
+
+# def add(*args):
+#     total = 0 
+#     for arg in args:
+#         total +- arg
+#     return total
+
+# print(add(1, 2, 3))
+
+# def display_name(*args):
+#     for arg in args:
+#         print(arg, end="()")
+
+# display_name("dr.", "spongebob", "squarepants", "III")
+
+
+# # KWARGS
+# def print_address(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key}: {value}")
+
+# print_address(street = "123 Fake St.",
+#               apt = "100",
+#               city = "Detroit", 
+#               state = "MI",
+#               zip = "54321")
+
+# ARGS AND KWARGS
+
+def shipping_label(*args, **kwargs):
+    for arg in args:
+        print(arg, end = " ")
+    print()
+    for value in kwargs.values():
+        print(value, end = " ")
+
+shipping_label("dr.", "spongebob", "squarepants", "III",
+            street = "123 Fake St.",
+            apt = "100",
+            city = "Detroit", 
+            state = "MI",
+            zip = "54321")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Indefinite Arguments (*args) Practice #1
+# Create a function called sum_squares that takes any number of numeric arguments, and returns the sum of their values squared.
+
+# For example for the arguments sum_squares(1,2,3) it should return 14 (1+4+9).
+
+
+# Indefinite Arguments (*args) Practice #2
+# Create a function called absolute_sum, which takes any number of arguments, and returns the sum of their absolute values (that is, it takes the non-negative values and adds them together, in other words, considers them all - negative and positive - as positive).
+
+# Indefinite Arguments (*args) Practice #3
+# Create a function called personal_numbers that receives, as its first argument, a name, and then an indefinite number of values.
+
+# The function should return the following message:
+
+# "{name}, the sum of your numbers is {sum_numbers}"
